b8eda4bb111a6b484400ff767369ad26abbe265c
sharedordaz <sharedordaz@live.com.mx>
2024-02-17 22:28:17 -0600

    Box shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import cursor from "../../public/cursor.png"
 
+
+
 export default function Home() {
  return(
  <div className={styles.welcomeDiv} style={{cursor:`url(${cursor.src}), crosshair`}}>
