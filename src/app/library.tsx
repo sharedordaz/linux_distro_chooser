7727d55878c5a1064f2a4e84c2e8f47b959132d6
sharedordaz <sharedordaz@live.com.mx>
2024-02-22 00:04:14 -0600

    Organized stuff

--- src/app/library.tsx
+++ src/app/library.tsx
@@ -1,11 +1,12 @@
 'use server'
 import { cookies } from 'next/headers';
+import { Answer } from './types';
 
 export default async function Library(){
     return(<p>Nothing</p>)
     }
 
-export async function checkAnswer(option: any){
+export async function checkAnswer(option: Answer){
             
          cookies().set(`Answer`, `${option.valueToSumTo}: ${option.value}`);
          console.log(cookies().get('Answer'));
