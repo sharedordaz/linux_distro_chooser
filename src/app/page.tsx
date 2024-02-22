a5e0e304f1e8be21547b5ca25593792b1b87a928
sharedordaz <sharedordaz@live.com.mx>
2024-02-21 20:57:38 -0600

    Some CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,32 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import cursor from "../../public/cursor.png"
 import Quiz from "./quiz";
 
+import { useState } from 'react';
+
+
+function displayQuiz(){
+        
+    }
 
 
 export default function Home() {
+    const [showQuiz, setShowQuiz] = useState(false);
+
+    const handleStartTest = () => {
+        setShowQuiz(true);
+        }
  return(
  <div className={styles.welcomeDiv} style={{cursor:`url(${cursor.src}), crosshair`}}>
     <h1>Linux Distro Chooser</h1>
     <p>Start choosing your distro today!</p>
     <br /> <br />
-    <button>Start Test</button>
-    <Quiz />
+    {!showQuiz && (
+        <button onClick={handleStartTest}>Start Test</button>
+    )}
+    {showQuiz && <Quiz />}
     
  </div>
  );
