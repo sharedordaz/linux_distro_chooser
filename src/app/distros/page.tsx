d2d4ef4e3818077058ca75147fe5c86f0ab39a8b
sharedordaz <sharedordaz@live.com.mx>
2024-02-23 00:12:25 -0600

    Distributions WORKING

--- src/app/distros/page.tsx
+++ src/app/distros/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Head from 'next/head';
 import Image from 'next/image';
-import distributionsData from '/public/distrosTest.json';
+import distributionsData from '../../../public/distrosTest.json';
 
 export default function Distributions() {
   return (
